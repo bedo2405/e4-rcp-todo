8a52fe28791d0d31fbf027d8fd380f92dc861f57
Philip Bedingfield <bedingfp@rta.nsw.gov.au>
2014-01-31 13:35:41 +1100

    Initial revision

---
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/parts/TodoOverviewPart.java
@@ -0,0 +1,10 @@
+package com.example.e4.rcp.todo.parts;
+
+public class TodoOverviewPart {
+
+	public TodoOverviewPart() {
+		super();
+		System.out.println("Constructing " + this.getClass().getSimpleName());
+	}
+
+}
